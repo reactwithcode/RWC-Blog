80bacea04eb6b1367759d96c3eb77f0f6c51ddd3
hari <thohari@atto.id>
2022-01-04 17:47:06 +0700

    time to ship it

---
+++ pages/index.js
@@ -0,0 +1,90 @@
+import PostFeed from '@components/PostFeed';
+import Metatags from '@components/Metatags';
+import Loader from '@components/Loader';
+import { firestore, fromMillis, postToJSON } from '@lib/firebase';
+
+import { useState } from 'react';
+
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+	const postsQuery = firestore
+		.collectionGroup('posts')
+		.where('published', '==', true)
+		.orderBy('createdAt', 'desc')
+		.limit(LIMIT);
+
+	const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+	return {
+		props: { posts }, // will be passed to the page component as props
+	};
+}
+
+export default function Home(props) {
+	const [posts, setPosts] = useState(props.posts);
+	const [loading, setLoading] = useState(false);
+
+	const [postsEnd, setPostsEnd] = useState(false);
+
+	// Get next page in pagination query
+	const getMorePosts = async () => {
+		setLoading(true);
+		const last = posts[posts.length - 1];
+
+		const cursor =
+			typeof last.createdAt === 'number'
+				? // convert to timestamp
+				  fromMillis(last.createdAt)
+				: last.createdAt;
+
+		const query = firestore
+			.collectionGroup('posts')
+			.where('published', '==', true)
+			.orderBy('createdAt', 'desc')
+			.startAfter(cursor)
+			.limit(LIMIT);
+
+		const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+		setPosts(posts.concat(newPosts));
+		setLoading(false);
+
+		if (newPosts.length < LIMIT) {
+			setPostsEnd(true);
+		}
+	};
+
+	return (
+		<main>
+			<Metatags
+				title="Home Page"
+				description="Get the latest posts on our site"
+			/>
+
+			<div className="card card-info">
+				<h2>💡 RWC Blog</h2>
+				<p>
+					Welcome! RWC Blog is a blogging web app that is built with Next.js and
+					Firebase.
+				</p>
+				<p>
+					You can sign up for an 🦸🏻‍♂️ account, ✍️ write posts, then give ❤️ likes
+					to the content created by other users. All public content is
+					server-rendered and search-engine optimized.
+				</p>
+			</div>
+
+			<PostFeed posts={posts} />
+
+			{!loading && !postsEnd && (
+				<button onClick={getMorePosts}>Load more</button>
+			)}
+
+			<Loader show={loading} />
+
+			{postsEnd && 'You have reached the end!'}
+		</main>
+	);
+}
